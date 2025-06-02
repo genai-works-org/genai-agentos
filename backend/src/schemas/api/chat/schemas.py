41e6649ab9ab41863cbdabd32e235b51df634c00
Valentyn Slivko <metallistvalon@gmail.com>
2025-06-02 16:10:45 +0300

    Fix: content field type

--- backend/src/schemas/api/chat/schemas.py
+++ backend/src/schemas/api/chat/schemas.py
@@ -1,15 +1,22 @@
+import json
 from datetime import datetime
 from typing import Optional
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from src.schemas.base import BaseUUIDToStrModel, CastSessionIDToStrModel
 from src.utils.enums import SenderType
 
 
 class BaseChatMessage(BaseModel):
     sender_type: SenderType
-    content: str
+    content: str | dict
+
+    @field_validator("content")
+    def cast_dict_to_json_str(cls, v):
+        if isinstance(v, dict):
+            return json.dumps(v)
+        return v
 
 
 class GetChatMessage(BaseChatMessage):
