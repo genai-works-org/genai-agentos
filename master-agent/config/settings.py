088b0ebacacc7ec000e2774b0089d0b9ddf3f72b
Andrii Krasnyh <andr.krasnyh@gmail.com>
2025-06-05 15:26:25 +0300

    Add default value of backend api key

--- master-agent/config/settings.py
+++ master-agent/config/settings.py
@@ -12,5 +12,8 @@ class Settings(BaseSettings):
         default="e1adc3d8-fca1-40b2-b90a-7b48290f2d6a::master_server_ml",
         alias="MASTER_AGENT_API_KEY",
     )
-    MASTER_BE_API_KEY: str = Field(..., alias="MASTER_BE_API_KEY")
+    MASTER_BE_API_KEY: str = Field(
+        default="30805518-19f3-49a6-9702-c0b0a6e87310::master_server_be",
+        alias="MASTER_BE_API_KEY"
+    )
     BACKEND_API_URL: str = Field(default="http://backend:8000/api", alias="BACKEND_API_URL")
